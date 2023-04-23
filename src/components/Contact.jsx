f984ec71b281574b0f48b85bc599c2bddae41cb3
kadershaikh <kadershaikh666@gmail.com>
2023-04-23 12:30:48 +0530

    contact form validations added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -21,18 +21,24 @@ const Contact = () => {
             className=" flex flex-col w-full md:w-1/2"
           >
             <input
+              maxlength="50"
+              required
               type="text"
               name="name"
               placeholder="Enter your name"
               className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             />
             <input
+              maxlength="50"
+              required
               type="text"
               name="email"
               placeholder="Enter your email"
               className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             />
             <textarea
+              maxlength="250"
+              required
               name="message"
               placeholder="Enter your message"
               rows="10"
